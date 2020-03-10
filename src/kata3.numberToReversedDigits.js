7d27b124f1228da7dc6a03bf2a52c85e4f1ced9a
Justin Sandahl <sandahl.j.d@gmail.com>
2020-03-10 20:13:24 +0000

    solved numberToReversedDigits, have left in commented out code that was using to test

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,10 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number => {
+    //const array = number.toString().split('') //.map(Number).reverse();
+    
+    //const array = [];
+    //const reverseNumber = 
+    return Array.from(String(number),Number).reverse(); 
+    //return reverseNumber;
+};
 
 module.exports = numberToReversedDigits;
