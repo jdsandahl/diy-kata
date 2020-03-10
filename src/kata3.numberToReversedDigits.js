50da9c90d012c2bc47d59dcf364ff4e28b6e1b41
Justin Sandahl <sandahl.j.d@gmail.com>
2020-03-10 20:22:01 +0000

    cleaned up numberToReversedDigits, two solutions provided one using Array.from and in-built functions String() and Number (commented out), the second is less abstract returning the number converted to a string and mapped to an array using Number function and then reversing the array

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,10 +1,6 @@
 const numberToReversedDigits = number => {
-    //const array = number.toString().split('') //.map(Number).reverse();
-    
-    //const array = [];
-    //const reverseNumber = 
-    return Array.from(String(number),Number).reverse(); 
-    //return reverseNumber;
-};
+    //return Array.from(String(number),Number).reverse(); 
+    return number.toString().split('').map(Number).reverse();
+}    
 
 module.exports = numberToReversedDigits;
