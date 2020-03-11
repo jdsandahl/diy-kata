3474d2d240ec02ba73885c1aadeef2611fd2ce1c
Justin Sandahl <sandahl.j.d@gmail.com>
2020-03-11 17:26:59 +0000

    solved humanCatDog years, using terinary operators, addition solution using if statements provided in comments

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,48 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+  const catAge =
+    number <= 0 || typeof number !== "number"
+      ? 0
+      : number === 1
+      ? 15
+      : number === 2
+      ? 15 + 9
+      : 24 + 4 * (number - 2);
+
+  const dogAge =
+    number < 0 || typeof number !== "number"
+      ? 0
+      : number === 1
+      ? 15
+      : number === 2
+      ? 15 + 9
+      : 24 + 5 * (number - 2);
+
+  return [number, catAge, dogAge];
+};
+//Solution using standard if statements below
+/*
+  let catYears;
+  let dogYears;
+  if (number <= 0 || typeof number !== "number") {
+    return [0, 0, 0];
+  }
+  if (number === 1) {
+    catYears = 15;
+    dogYears = 15;
+  }
+  if (number === 2) {
+    catYears = 15 + 9;
+    dogYears = 15 + 9;
+  }
+  if (number > 2) {
+    catYears = 16 + 4 * number;
+    dogYears = 14 + 5 * number;
+  }
+
+  return [number, catYears, dogYears];
+};
+*/
+
+
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
 const { humanCatDogYears } = require("../src");
 
+describe("humanCatDogYears", () => {
+    it("Returns Human, Cat and Dog years accurately", () => {
+        expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    });
+});
+
 // Look Ma, no handlebars!!!
