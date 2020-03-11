52efca2c5bd89ba1c86ade9a315e2eb5bf729ea2
Justin Sandahl <sandahl.j.d@gmail.com>
2020-03-11 18:12:13 +0000

    completed reachDestination

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,6 @@
-const reachDestination = (distance, speed) => {};
-
+const reachDestination = (distance, speed) => {
+    const eTA = distance / speed; 
+    return `I should be there in ${Math.ceil(eTA*2)/2} hours`;
+};
+    
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,11 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toBe("I should be there in 4.5 hours");
+    expect(reachDestination(33, 10)).toBe("I should be there in 3.5 hours");
+    expect(reachDestination(45, 10)).toBe("I should be there in 4.5 hours");
+    expect(reachDestination(56, 10)).toBe("I should be there in 6 hours");
+    expect(reachDestination(0, 10)).toBe("I should be there in 0 hours");
+  });
 });
