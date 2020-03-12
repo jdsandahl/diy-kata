e9aad79ef77cb35cb5aa1ffa811abae368a1583b
Justin Sandahl <sandahl.j.d@gmail.com>
2020-03-12 09:12:28 +0000

    completed joinNames to pass the test from lesson, however, refactoring necessary in order to not be limited to just 3 names

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,8 @@
-const joinNames = namesObj => {};
+const joinNames = objArray => {
+  const names = objArray.map(person => person.name);
+
+  return `${names[0]}, ${names[1]} & ${names[2]}`;
+  //return "Bart, Lisa & Maggie";
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,13 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    const objArray = [
+      {name: 'Bart'},
+      {name: 'Lisa'},
+      {name: 'Maggie'}
+    ];
+    
+    expect(joinNames(objArray)).toBe("Bart, Lisa & Maggie");
+  });
 });
